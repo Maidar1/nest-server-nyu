a07ece898a47bc570140410f49107d86f8eacb87
Maidar <maidarotgonn@gmail.com>
2023-01-11 21:15:24 +0800

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { DataModule } from './data/data.module';
 
 @Module({
   imports: [
-    UserModule,
     TypeOrmModule.forRoot({
       type: 'mongodb',
       url: 'mongodb+srv://admin:6mrVWMpotsj2rv6o@cluster0.ufhhfgk.mongodb.net/?retryWrites=true&w=majority',
       entities: [join(__dirname, '**/**.entity{.ts,.js}')],
     }),
+    DataModule,
   ],
   controllers: [AppController],
   providers: [AppService],
